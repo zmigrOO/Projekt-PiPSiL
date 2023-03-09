8a5b7c631ddb79f1f988e39801e54e0d2c67acb9
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2023-03-09 12:11:51 +0100

    changed view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+    </head>	<body>
+		<h1>Hej </h1>
+	</body>
+</html>
