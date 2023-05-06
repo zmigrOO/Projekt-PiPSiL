5a7f1325fde638b4c522bb3722cfb6e039dfc27b
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2023-05-06 20:25:46 +0200

    user now can't watch his own offers

--- resources/views/components/offer.blade.php
+++ resources/views/components/offer.blade.php
@@ -29,57 +29,61 @@
 
 
 
-    <div class="flex font-sans py-2">
-        <div class="flex-none w-56 relative">
+    <div class="flex font-sans py-2 sm:max-h-fit" style="">
+        <div class="flex-none w-56 relative" style="max-width: 30vw">
             <a href="/offers/{{ $offer->id }}">
-            <img src="/images/samolot.bmp" alt="" class="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
-        </a>
+                <img src="/images/samolot.bmp" alt=""
+                    class="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
+            </a>
         </div>
-  <form class="flex-auto p-6">
-    <div class="flex flex-wrap">
-      <h1 class="flex-auto font-medium text-4xl p-6 text-gray-900 dark:text-gray-100 ">
-            <a href="/offers/{{ $offer->id }}">
+        <form class="flex-auto p-6">
+            <div class="flex flex-wrap">
+                <h1 class="flex-auto font-medium text-4xl p-6 text-gray-900 dark:text-gray-100 ">
+                    <a href="/offers/{{ $offer->id }}">
 
-        {{ $offer->name }}
-    </a>
-      </h1>
-      <div class="w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600">
-        ${{ $offer->price }}
-      </div>
-      <div class="text-sm font-medium text-slate-400">
-        Available: {{ $offer->quantity }} items
-      </div>
+                        {{ $offer->name }}
+                    </a>
+                </h1>
+                <div class="w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600">
+                    ${{ $offer->price }}
+                </div>
+                <div class="text-sm font-medium text-slate-400">
+                    Available: {{ $offer->quantity }} items
+                </div>
+            </div>
+            <div class="float-right flex space-x-4 mb-5 text-sm font-medium">
+
+
+                {{-- <a href="/watch/{{ $offer->id }}"><img class="dark:invert" src="@if ($offer->watched == true)fav.svg @else nfav.svg @endif" alt="favourite" class="w-5 h-5"></a> --}}
+                {{-- if user is authenticated --}}
+                @auth @if(Auth::user()->id == $offer->user_id)
+                    <a style="cursor: pointer;"
+                        onclick="watchOffer({{ $offer->id }}, document.getElementById('img{{ $offer->id }}'))">
+                        <img id="img{{ $offer->id }}" class="dark:invert"
+                            src="@if ($offer->watched == true) fav.svg @else nfav.svg @endif" alt="favourite"
+                            class="w-5 h-5">
+                    </a>
+                @endauth @endif
+            </div>
+
+        </form>
     </div>
-    <div class="float-right flex space-x-4 mb-5 text-sm font-medium">
-
-
-        {{-- <a href="/watch/{{ $offer->id }}"><img class="dark:invert" src="@if($offer->watched==true)fav.svg @else nfav.svg @endif" alt="favourite" class="w-5 h-5"></a> --}}
-        {{-- if user is authenticated --}}
-        @auth
-        <a style="cursor: pointer;" onclick="watchOffer({{ $offer->id }}, document.getElementById('img{{$offer->id}}'))">
-    <img id="img{{ $offer->id }}" class="dark:invert" src="@if($offer->watched==true)fav.svg @else nfav.svg @endif" alt="favourite" class="w-5 h-5">
-        </a>
-        @endauth
-    </div>
-
-  </form>
-</div>
 </div>
 <script>
-function watchOffer(offerId, img) {
-    fetch('/watch/' + offerId)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            // update the image src based on the response
-            if (response.watched == true) {
-                img.src = 'fav.svg';
-                // console.log('fav.svg');
-            } else {
-                img.src = 'nfav.svg';
-                // console.log('nfav.svg');
-            }
-        });
-}
+    function watchOffer(offerId, img) {
+        fetch('/watch/' + offerId)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                // update the image src based on the response
+                if (response.watched == true) {
+                    img.src = 'fav.svg';
+                    // console.log('fav.svg');
+                } else {
+                    img.src = 'nfav.svg';
+                    // console.log('nfav.svg');
+                }
+            });
+    }
 </script>
