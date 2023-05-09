1da815a0e1b11d593a1e36d058034f7230aa94c9
zmigrOO <100092805+zmigrOO@users.noreply.github.com>
2023-05-09 13:34:00 +0200

    Revert "Merge pull request #10 from zmigrOO/SEO"
    This reverts commit 1e24c5b88e7171493ca5f2b73664e3642accfe37, reversing
    changes made to 275a850a331816f3dc8175850b0b2a43e6562c26.

--- resources/views/components/noauth.blade.php
+++ resources/views/components/noauth.blade.php
@@ -10,42 +10,35 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @include('meta::manager', [
-    'title'         => 'Available offers - Shop Now!',
-    'description'   => 'Find the best deals and discounts on a wide range of products at our online store. Shop now and get great savings on electronics, fashion, home goods, and more. Fast and reliable shipping available. Shop now and start saving!',
-    'author'        => 'Patryk Żmigrodzki, Aleksandra Kozubal, Natalia Wieczorek',
-    'keywords'      => 'offers, discounts, deals, electronics, fashion, home goods',
-    'geo_region'    => 'PL',
-])
 </head>
 
 <body class="font-sans w-full antialiased relative dark:bg-grey-900">
-<div>
-    <div
-        class="z-10 mx-auto px-4 abs sm:px-6 top-0 left-0 fixed w-full lg:px-8 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 z-20 flex items-center">
-                    <a href="{{ route('offers') }}">
-                        <x-application-logo
-                            class="block h-9 w-auto z-30 fill-current text-teal-500 dark:text-gray-200"/>
-                    </a>
+    <div>
+        <div
+            class="z-10 mx-auto px-4 abs sm:px-6 top-0 left-0 fixed w-full lg:px-8 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 z-20 flex items-center">
+                        <a href="{{ route('offers') }}">
+                            <x-application-logo
+                                class="block h-9 w-auto z-30 fill-current text-teal-500 dark:text-gray-200" />
+                        </a>
+                    </div>
+
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    </div>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                </div>
+                <!-- Hamburger -->
             </div>
-
-
-            <!-- Hamburger -->
         </div>
-    </div>
+
         <div class="min-h-screen bg-gray-100 abs dark:bg-gray-900 fix">
             @if (Route::has('login'))
                 <div class="fixed top-0 z-20 right-0 p-6 text-right">
@@ -65,15 +58,11 @@
                 </div>
             @endif
 
-        <!-- Responsive Text Input and Submit Button -->
-        @if(Route::currentRouteName() == 'offers')
-            <form action="/search" method="get"
-                  class="p-4 text-center z-0 sm:z-20 relative sm:fixed top-16 sm:top-0 -translate-x-1/2 w-full sm:w-auto"
-                  style="left: 50%">
+            <!-- Responsive Text Input and Submit Button -->
+            @if(Route::currentRouteName() == 'offers')
+            <form action="/search" method="get" class="p-4 text-center z-0 sm:z-20 relative sm:fixed top-16 sm:top-0 -translate-x-1/2 w-full sm:w-auto" style="left: 50%">
                 <div class="flex-none float-left w-fit">
                     <x-text-input id="search" class="sm:w-80 h-10" type="text" name="search" autofocus
-
-
                         placeholder="{{ __('Search') }}" />
                 </div>
                 <div class="flex-none float-right">
@@ -82,14 +71,14 @@
                     </x-primary-button>
                 </div>
             </form>
-        @endif
+            @endif
 
-        <!-- Page Content -->
-        <main class="mt-16 sm:mt-16">
-            {{ $slot }}
-        </main>
+            <!-- Page Content -->
+            <main class="mt-16 sm:mt-16">
+                {{ $slot }}
+            </main>
+        </div>
     </div>
-</div>
 </body>
 
 </html>
